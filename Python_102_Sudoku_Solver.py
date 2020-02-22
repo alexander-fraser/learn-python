f33f343080cc599e20b86ecdefbe1e270341e441
alexander-fraser <alexfraser4@gmail.com>
2020-02-22 10:12:34 -0500

    Include solver logic.

--- Python_102_Sudoku_Solver.py
+++ Python_102_Sudoku_Solver.py
@@ -4,9 +4,17 @@
 
 # Solves Sudoku puzzles using a recursive algorithm.
 
+import numpy as np
+
 def main():
     unsolved_puzzle = import_sudoku()
-    solved_puzzle = sudoku_solver(unsolved_puzzle)
+    print(np.asarray(unsolved_puzzle))
+    
+    global puzzle
+    puzzle = unsolved_puzzle
+    
+    solved_puzzle = sudoku_solver()
+    print(np.asarray(solved_puzzle))
 
 def import_sudoku():
     # Asks the user to enter the sudoku puzzle as a series of 81 integers.
@@ -14,7 +22,7 @@ def import_sudoku():
     # The function outputs the puzzle as a list.
 
     #sudoku_string = input("Pass the sudoku values from top-left to bottom-right, with 0s for blank cells:\n")
-    sudoku_string = "123456789223456789323456789423456789523456789623456789723456789823456789923456789"
+    sudoku_string = "530070000600195000098000060800060003400803001700020006060000280000419005000080079"
 
     sudoku_list = []
 
@@ -22,7 +30,7 @@ def import_sudoku():
         sudoku_row = []
 
         for j in range(9):
-            sudoku_row.append( sudoku_string[(9*i + j)] )
+            sudoku_row.append( int(sudoku_string[(9*i + j)] ))
         sudoku_list.append(sudoku_row)
 
     #print(sudoku_string)
@@ -30,8 +38,45 @@ def import_sudoku():
 
     return sudoku_list
 
-def sudoku_solver(unsolved_puzzle):
+def check_valid(puzzle, row, col, value):
+    # Checks whether the given value is a valid option for the specified cell,
+    # given the rules of Sudoku.
+
+    # Check rows.
+    for i in range(9):
+        if puzzle[row][i] == value:
+            return False
+
+    # Check columns.
+    for i in range(9):
+        if puzzle[i][col] == value:
+            return False
+
+    # Check square.
+    square_row = int(row / 3)
+    square_col = int(col / 3)
+
+    for i in range(3):
+        for j in range(3):
+            if puzzle[square_row * 3 + i][square_col * 3 + j] == value:
+                return False
+
+    return True
+
+def sudoku_solver():
+    for row in range(9):
+        for col in range(9):
+            if puzzle[row][col] == 0:
+                
+                for value in range(9):
+                    if check_valid(puzzle, row, col, value):
+                        puzzle[row][col] = value
+                        print(row, col, value)
+                        sudoku_solver()
+                        puzzle[row][col] = 0
+                return
     
+    return puzzle
 
 if __name__ == "__main__":
     main()
