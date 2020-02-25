5f6f30eb311d42a4c3dfd1c44f48f7b311455267
alexander-fraser <alexfraser4@gmail.com>
2020-02-24 21:10:15 -0500

    Refactor variable and function names.

--- Python_007_List_Overlap.py
+++ Python_007_List_Overlap.py
@@ -14,31 +14,34 @@ lists of different sizes.
 
 from random import randint
 
-def generateRandomList():
+def generate_random_list():
+    # Generates a list of random length (1-20 elements) of integers (1-9).
+    input_list = []
+    list_length = randint(1,20)
 
-    inputList = []
-    listLength = randint(1,20)
+    for _ in range(0, list_length):
+        input_list.append(randint(1,10))
 
-    for x in range(0, listLength):
-        inputList.append(randint(1,10))
+    return input_list
 
-    return inputList
-
-def compareLists(list1, list2):
-    outputList = []
+def compare_lists(list1, list2):
+    # Outputs a list of the elements common between the two input lists.
+    output_list = []
 
     for element in list1:
-        if (element in list2) and (element not in outputList):
-            outputList.append(element)
+        if (element in list2) and (element not in output_list):
+            output_list.append(element)
 
-    outputList = sorted(outputList)
-    return outputList
+    output_list = sorted(output_list)
+    return output_list
 
 def main():
-    list1 = generateRandomList()
-    list2 = generateRandomList()
-    outputList = compareLists(list1, list2)
-    print(list1, "\n", list2, "\n", outputList)
+    # Generates two lists of random length and with random integers.
+    # Then outputs the elements that are common between the two lists.
+    list1 = generate_random_list()
+    list2 = generate_random_list()
+    output_list = compare_lists(list1, list2)
+    print(list1, "\n", list2, "\n", output_list)
 
 if __name__ == "__main__":
     main()
