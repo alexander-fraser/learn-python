2b1b93b8820705c43df2b1df0d04fc2fd7310428
alexander-fraser <alexfraser4@gmail.com>
2020-03-05 20:55:14 -0500

    Add documentation and user input for puzzle height.

--- Python_106_Tower_of_Hanoi.py
+++ Python_106_Tower_of_Hanoi.py
@@ -5,33 +5,62 @@
 # Solves a Tower of Hanoi puzzle using a recursive algorithm.
 
 """
-
-Notes
+Tower of Hanoi puzzle instructions:
 - There are three sticks: A, B, and C.
-- The disks all start on stick A. The goal is to get them to C.
-- Disks can only stack in the order they start (i.e. small to large).
-- For each disk, you need to get the disks above onto B, so that you 
-can move that disk to C.
-- There is a recursive element then. The process is the same for 3 or 100 disks.
+- There are a specified number of disks arranged in ascending size.
+- The disks all start on A. The goal is to get them all to C.
+- Disks can only stack in the order they started (i.e. small to large).
 
+This Tower of Hanoi solver uses a recursive algorithm. That is,
+there is a function that calls itself to solve a simpler-and-simpler
+version of the puzzle, eventually getting down to the base case of 0 disks
+(where it does nothing).
+
+The solver works by:
+1. Moving all disks above the specified disk to a placeholder.
+2. Then moving the specified disk to the "to" place.
+3. Finally, moving the disks back on top of the specified disk.
 """
 
 
+def collect_integer(user_message, lower_limit, upper_limit):
+    # This function prompts the user for an integer.
+    # It loops until an integer is entered by the user that satisfies the limits.
+    while True:
+        try:
+            user_input = input(user_message + "\n")
+            user_value = int(user_input)
+        except:
+            print("That was not a valid integer. Please try again.")
+        else:
+            if (user_value >= lower_limit) and (user_value <= upper_limit):
+                break
+            print("The integer was not between {} and {}. Please try again.".format(lower_limit, upper_limit))
+
+    return user_value
+
+
 def print_move(move_from, move_to):
+    # Prints the instructions for solving the puzzle.
     print("Move disk from {} to {}.".format(move_from, move_to))
 
 
 def hanoi_move(disks, move_from, move_via, move_to):
-    if disks == 0:
-        pass
-    else:
+    # Recursive loop that solves the puzzle.
+    # Moves all disks above the specified disk to a placeholder.
+    # Then moves the specified disk to the "to" place.
+    # Finally moves the disks above it back on top of the specified disk.
+    if disks != 0:
         hanoi_move(disks-1, move_from, move_to, move_via)
         print_move(move_from, move_to)
         hanoi_move(disks-1, move_via, move_from, move_to)
 
 
 def main():
-    disks_number = 3
+    # Prompts the user to specify the puzzle height.
+    # Then calls the solver to print the instructions.
+    print("Welcome to the Tower of Hanoi solver.")
+    disks_number = collect_integer("How many disks are in the tower?", 1, 100)
     hanoi_move(disks_number, "A", "B", "C")
 
 
