e94f57a2fb5f26745644adf0fa916a7cb8d215a5
Alexander Fraser <alexfraser4@gmail.com>
2026-05-28 20:44:51 -0400

    Include input check.

--- Python_402_Build_Your_Own_Pizza.py
+++ Python_402_Build_Your_Own_Pizza.py
@@ -7,7 +7,10 @@ base_list = {"C": 0, "T": 1, "D": 2}
 service_list = {"T": 0, "E": 2}
 discount_code = "P1ZZ4"
 
-def collect_input():
+def collect_input(retry_flag):
+    if retry_flag == 1:
+        print("The inputs you entered were invalid. Please try again.")
+
     size = input("Pick a size: [S]mall=$6, [M]edium=$8, [L]arge=$10: ")
     base = input("Choose your base: [C]lassic=$0, [T]hin=$1, [D]eep=$2: ")
     toppings = input("How many toppings would you like ($1.50 each up to "
@@ -19,14 +22,28 @@ def collect_input():
 
     return client_inputs
 
+def check_input(client_inputs):
+    input_indicator = 0
+    if client_inputs[0] not in size_list:
+        input_indicator = 1
+    if client_inputs[1] not in base_list:
+        input_indicator = 1
+    try:
+        val = int(client_inputs[2])
+        if val not in range(0,5):
+            input_indicator = 1
+    except:
+        input_indicator = 1
+    if client_inputs[3] not in service_list:
+        input_indicator = 1
+        
+    return input_indicator
+
 def calculate_cost(client_inputs):
     total_cost = 0
     total_cost = total_cost + size_list[client_inputs[0]]
     total_cost = total_cost + base_list[client_inputs[1]]
-    if int(client_inputs[2]) < 5:
-        total_cost = total_cost + int(client_inputs[2]) * 1.5 
-    else:
-        total_cost = total_cost + 4 * 1.5
+    total_cost = total_cost + int(client_inputs[2]) * 1.5 
     total_cost = total_cost + service_list[client_inputs[3]]
     if client_inputs[4] == discount_code:
         total_cost = total_cost * 0.8
@@ -34,10 +51,19 @@ def calculate_cost(client_inputs):
     return total_cost
 
 def main():
+    client_inputs = []
+    input_indicator = 1
+    retry_flag = 0
+
     print("Build Your Own Pizza")
-    client_inputs = collect_input()
+    while input_indicator == 1:
+        client_inputs.clear()
+        client_inputs = collect_input(retry_flag)
+        input_indicator = check_input(client_inputs)
+        retry_flag = 1
+
     total_cost = calculate_cost(client_inputs)
-    print("Your pizza costs: $" + str(total_cost))
+    print("Your pizza costs: $%.2f" % total_cost)
 
 if __name__ == "__main__":
     main()
