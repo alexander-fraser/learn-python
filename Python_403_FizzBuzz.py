fe58894c101a8e46cdd2963f337265409ded139a
Alexander Fraser <alexfraser4@gmail.com>
2026-05-31 19:52:09 -0400

    Modify to use map.

--- Python_403_FizzBuzz.py
+++ Python_403_FizzBuzz.py
@@ -7,6 +7,12 @@ def collect_input():
     size = int(size_str)
     return size
 
+def create_list(size):
+    input_list = []
+    for value in range(1, size+1):
+        input_list.append(value)
+    return input_list
+
 def determine_fizzbuzz(value):
     fizz = 0
     buzz = 0
@@ -28,11 +34,16 @@ def determine_fizzbuzz(value):
     else:
         return str(value)
 
+def process_output(input_list):
+    output_list = map(determine_fizzbuzz, input_list)
+    return output_list
+
 def main():
     size = collect_input()
-    for value in range(0, size+1):
-        output_text = determine_fizzbuzz(value)
-        print(output_text)
+    input_list = create_list(size)
+    print(list(input_list))
+    output_list = process_output(input_list)
+    print(list(output_list))
 
 if __name__ == "__main__":
     main()
