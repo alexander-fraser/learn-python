69345fe55d196dd96d4130c1582cfdb46ce1aa01
Alexander Fraser <alexfraser4@gmail.com>
2026-07-12 16:04:12 -0400

    410 - Create find missing number snippet.

---
+++ Python_410_Find_Missing_Number.py
@@ -0,0 +1,41 @@
+# Find the Missing Number
+# Alexander Fraser
+# 11 July 2026
+
+import random
+
+def setup():
+    input_end = input("Creating a list of integers from 0 to n. n=")
+    input_end = int(input_end)
+
+    input_list = []
+    for integer in range(0, input_end + 1):
+        input_list.append(integer)
+
+    input_missing = random.randint(0, input_end)
+    input_list.pop(input_missing)
+
+    return input_list
+
+def find_missing_sum(input_list):
+    list_sum = 0
+    for integer in input_list:
+        list_sum += integer
+
+    list_length = len(input_list)
+    full_sum = 0
+    for integer in range(0, list_length + 1):
+        full_sum += integer
+
+    output_missing = full_sum - list_sum
+    return output_missing
+    
+def main():
+    print("Welcome to the 'find the missing number' program.")
+    input_list = setup()
+    print(input_list)
+    output_missing = find_missing_sum(input_list)
+    print(output_missing)
+
+if __name__ == "__main__":
+    main()
