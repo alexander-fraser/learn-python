55a1903402a767584d901af6645b36316af7782a
Alexander Fraser <alexfraser4@gmail.com>
2026-07-12 16:11:06 -0400

    410 - Change from sum to XOR algorithm.

--- Python_410_Find_Missing_Number.py
+++ Python_410_Find_Missing_Number.py
@@ -30,11 +30,21 @@ def find_missing_sum(input_list):
     output_missing = full_sum - list_sum
     return output_missing
     
+def find_missing_xor(input_list):
+    test_list = []
+    list_length = len(input_list)
+    for integer in range(0, list_length + 1):
+        test_list.append(integer)
+
+    output_missing = list(set(test_list) ^ set(input_list))
+    return output_missing
+
 def main():
     print("Welcome to the 'find the missing number' program.")
     input_list = setup()
     print(input_list)
-    output_missing = find_missing_sum(input_list)
+#    output_missing = find_missing_sum(input_list)
+    output_missing = find_missing_xor(input_list)
     print(output_missing)
 
 if __name__ == "__main__":
