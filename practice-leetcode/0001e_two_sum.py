56ce3b58390bc20b0beb0fe10199e8129034a9a5
Alexander Fraser <alexfraser4@gmail.com>
2026-08-02 16:22:10 -0400

    Create 0001 Two Sum script using nested loop algorithm.

---
+++ practice-leetcode/0001e_two_sum.py
@@ -0,0 +1,47 @@
+# 0001 Two Sum
+# Alexander Fraser
+# 2 August 2026
+
+"""
+You are given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+"""
+
+def two_sum(nums: list[int], target: int) -> list[int]:
+    for index1, value1 in enumerate(nums):
+        for index2 in range(index1 + 1, len(nums)):
+            value2 = nums[index2]
+            if (value1 + value2) == target:
+                return [index1, index2]
+
+    raise ValueError("No two numbers sum to the target.")
+
+def main() -> None:
+    for index, test in enumerate(test_cases):
+        result = two_sum(test["nums"], test["target"])
+        assert result == test["output"], (
+            f"Test {index} failed. "
+            f"Expected: {test["output"]}. "
+            f"Realized: {result}."
+        )
+        print(f"Test {index} passed.")
+
+test_cases = [
+    {
+        "nums": [2,7,11,15], 
+        "target": 9,
+        "output": [0,1]
+    },   
+    {
+        "nums": [2,7,11,15], 
+        "target": 9,
+        "output": [0,1]
+    },   
+    {
+        "nums": [2,7,11,15], 
+        "target": 9,
+        "output": [0,1]
+    }   
+]
+
+if __name__ == "__main__":
+    main()
